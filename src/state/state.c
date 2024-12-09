2d893229ce76d019b7998f685083cd071fb07bb5
Nadia Chambers <nadia.chambers@iohk.io>
2024-12-09 10:12:04 -0500

    check for allocation failure

--- src/state/state.c
+++ src/state/state.c
@@ -14,6 +14,8 @@ setup_state(struct c_trace_fwd_state **state, struct c_trace_fwd_conf *conf)
 	    page_size, retval = RETVAL_FAILURE;
 
 	*state = calloc(1, sizeof(struct c_trace_fwd_state));
+	if (!*state)
+		return RETVAL_FAILURE;
 	(*state)->unix_sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
 	if ((*state)->unix_sock_fd == -1)
 		goto exit_failure;
