fc76ffa0c146bb2006b3d1e756ba33cf15fcc1e4
Nadia Chambers <nadia.chambers@iohk.io>
2025-01-10 07:19:57 -0500

    use cbor_load vs. writing my own driver
    Getting rid of the code or cleaning things up so it's out of the main
    path can be saved for later.

--- src/service/service.c
+++ src/service/service.c
@@ -49,14 +49,18 @@ int service_loop(struct c_trace_fwd_state *state, struct c_trace_fwd_conf *conf)
 	int retval = RETVAL_FAILURE;
 
 	while (1) {
-		char *buf = calloc(1024, 1024);
+		char *buf;
 		ssize_t bytes_returned;
-		size_t bytes_to_decode;
+		size_t bytes_to_decode, buf_len,
+		       buf_chunksz = 1024, buf_chunks = 1024;
+		struct cbor_load_result result;
 		cbor_data cbor_buf;
-		struct cbor_decoder_result result;
+		cbor_item_t *item;
 
+		buf_len = buf_chunks * buf_chunksz;
+		buf = calloc(buf_chunks, buf_chunksz);
 retry_read:
-		bytes_returned = read(state->unix_sock_fd, buf, 1024 * 1024);
+		bytes_returned = read(state->unix_sock_fd, buf, buf_len);
 		if (bytes_returned < 0) {
 			if (errno != EAGAIN && errno != EWOULDBLOCK)
 				goto exit_failure;
@@ -65,16 +69,21 @@ retry_read:
 		}
 		bytes_to_decode = (size_t)bytes_returned;
 		cbor_buf = (cbor_data)buf;
-		result = cbor_stream_decode(cbor_buf, bytes_to_decode,
-					    &ctf_cbor_drv, state /* context */);
-		switch (result.status) {
-		case CBOR_DECODER_FINISHED:
+		memset(&result, 0, sizeof(struct cbor_load_result));
+		item = cbor_load(cbor_buf, bytes_to_decode, &result);
+		ctf_stk_push(state, item);
+		ctf_tbl_push(state, item);
+		switch (result.error.code) {
+		case CBOR_ERR_NONE:
 			service_relay(state, conf);
 			/* deliberate fall-through */
-		case CBOR_DECODER_NEDATA:
+		case CBOR_ERR_NOTENOUGHDATA:
 			/* break the switch, not loop */
 			break;
-		case CBOR_DECODER_ERROR:
+		case CBOR_ERR_NODATA:
+		case CBOR_ERR_MALFORMATED:
+		case CBOR_ERR_MEMERROR:
+		case CBOR_ERR_SYNTAXERROR:
 			goto exit_failure;
 		}
 
