4e4ec739a0505db054f5ae13db4f7b196a36eca8
Nadia Chambers <nadia.chambers@iohk.io>
2025-05-16 00:50:28 +0200

    add help text
    While throwing optparse-applicative at it, one might as well.

--- test/trace-compare.hs
+++ test/trace-compare.hs
@@ -9,8 +9,9 @@ import qualified "filepath" System.FilePath as
   FilePath (splitFileName)
 
 import qualified "optparse-applicative" Options.Applicative as
-  Options (Completer, Parser, ParserInfo, completer, execParser
-          , fullDesc, info, mkCompleter, strArgument)
+  Options (ArgumentFields, Completer, InfoMod, Mod, Parser, ParserInfo
+          , completer, execParser, info, metavar, mkCompleter, progDesc
+          , strArgument)
 
 import qualified "trace-compare" Trace.Forward.Test.CCodec as
   CCodec (diffFileSDUs)
@@ -21,15 +22,22 @@ import qualified "transformers-except" Control.Monad.Trans.Except.Extra as
 tcComplete :: Options.Completer
 tcComplete = Options.mkCompleter \(FilePath.splitFileName -> (dir, pfx)) -> do
   dirEnts <- Dir.listDirectory dir
-  pure $ [dir </> d | d <- dirEnts, pfx `List.isPrefixOf` d]
+  pure [dir </> ent | ent <- dirEnts, pfx `List.isPrefixOf` ent]
 
 tcInfo :: Options.ParserInfo (FilePath, FilePath)
-tcInfo = Options.info options Options.fullDesc
+tcInfo = options `Options.info` tcInfoMod
+
+tcInfoMod :: Options.InfoMod (FilePath, FilePath)
+tcInfoMod = Options.progDesc "compare trace SDU decodings"
+
+tcMod :: Bool -> Options.Mod Options.ArgumentFields FilePath
+tcMod b = Options.completer tcComplete <> Options.metavar txt where
+  txt = "FILE" <> if not b then "1" else "2"
 
 options :: Options.Parser (FilePath, FilePath)
 options = do
-  path1 <- Options.strArgument $ Options.completer tcComplete
-  path2 <- Options.strArgument $ Options.completer tcComplete
+  path1 <- Options.strArgument $ tcMod False
+  path2 <- Options.strArgument $ tcMod True
   pure (path1, path2)
 
 main :: IO ()
