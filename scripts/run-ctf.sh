b002bb19889bd166b3f594115c413fb196363695
Nadia Chambers <nadia.chambers@iohk.io>
2025-07-11 19:09:10 +0100

    --show-leak-kinds=all + line breaks/continuations

--- scripts/run-ctf.sh
+++ scripts/run-ctf.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-exec env LD_PRELOAD=./obj/lib/libc_trace_fwd.so LD_LIBRARY_PATH=$(pwd)/obj/lib:/lib:/usr/lib valgrind --tool=memcheck --leak-check=full --track-origins=yes ./obj/bin/c_trace_fwd -f $(realpath ../tracer-repl-mod/mainnetsingle/socket/tracer.socket) -u 127.0.0.1:9001
+SOCK_FILE=$(realpath ../tracer-repl-mod/mainnetsingle/socket/tracer.socket)
+exec env LD_PRELOAD=./obj/lib/libc_trace_fwd.so \
+	LD_LIBRARY_PATH=$(pwd)/obj/lib:/lib:/usr/lib \
+	valgrind --tool=memcheck \
+		--leak-check=full \
+		--show-leak-kinds=all \
+		--track-origins=yes \
+		./obj/bin/c_trace_fwd \
+			-f $SOCK_FILE \
+			-u 127.0.0.1:9001
