af9940ae1b5454c0bc93d4ac7beed999d2f5405f
Nadia Chambers <nadia.chambers@iohk.io>
2025-07-25 13:22:04 +0200

    define wild pointer check for later use

--- incl/ctf_util.h
+++ incl/ctf_util.h
@@ -2,6 +2,7 @@
 
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <unistd.h>
 #include "agency.h"
 
@@ -15,12 +16,26 @@ struct cbor_item_t;
 int ctf_msg_core(const struct ctf_msg_ctx *, const char *, ...);
 size_t cbor_refcount(const struct cbor_item_t *);
 
+#define CTF_CHK_PTR_LO_ADDR	(((uintptr_t)1) << 20)
+#define ctf_check_ptr(mod, ptr)					   \
+	({							   \
+		void *__ctx_ptr##__LINE__ = (ptr);		   \
+		bool __ctx_ret##__LINE__			   \
+		    = !!((uintptr_t)__ctx_ptr##__LINE__		   \
+					< CTF_CHK_PTR_LO_ADDR);	   \
+		if (__ctx_ret##__LINE__)			   \
+			ctf_msg(mod, "wild pointer %s = %p\n",	   \
+					#ptr,			   \
+					__ctx_ptr##__LINE__);	   \
+		__ctx_ret##__LINE__;				   \
+	})
+
 #define ctf_cbor_decref(mod, cbor)				   \
 	do {							   \
 		size_t __ctx_rc##__LINE__ = cbor_refcount(*(cbor));\
-		if ((unsigned long)(cbor) < (1UL << 20))	   \
+		if (ctf_check_ptr(mod, cbor))			   \
 			ctf_msg(mod, "wild ptr decref %s (%p/<addr not fetched>)\n", #cbor, cbor);\
-		else if ((unsigned long)(*(cbor)) < (1UL << 20))   \
+		else if (ctf_check_ptr(mod, *(cbor)))		   \
 			ctf_msg(mod, "wild ptr decref of what %s points to (%p/%p)\n", #cbor, cbor);\
 		else if (!__ctx_rc##__LINE__)			   \
 			ctf_msg(mod, "refcount zero on %s (%p/%p)\n", #cbor, cbor, *(cbor));\
