959eabe64d0db773f4341292cb1ee9328f982f38
Nadia Chambers <nadia.chambers@iohk.io>
2025-08-25 16:07:50 +0200

    route self-ref to cfg path in cfg through tmpfile
    The config file having to change every time its own path changes is a
    headache. The solution is to set the field of a template config with jq
    & put the generated config file in a tmpfile. It doesn't really matter
    that this might create numerous redundant config files.

--- scripts/tracer.sh
+++ scripts/tracer.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+SCRIPTS_DIR=$(dirname $(realpath $0))
+NODE_DIR=$(pwd)
+TMPFILE=$(mktemp tracerconfig.XXXXXXXXXX)
+jq '.["network"].["contents"]="'${TMPFILE}'"' \
+	< ${SCRIPTS_DIR}/tracerconfig-mainnet.sh.json > ${TMPFILE}
 exec cabal --jobs=256 --keep-going --disable-documentation \
 	run cardano-tracer:exe:cardano-tracer -- \
-	-c ./empty_dir/tracerconfig-mainnet.sh.json
+	-c ${TMPFILE}
