da8d9a7d083018198e0e56f39d101a77e27744b8
Nadia Chambers <nadia.chambers@iohk.io>
2025-08-27 13:44:37 +0200

    fix typo

--- scripts/tracer.sh
+++ scripts/tracer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SCRIPTS_DIR=$(dirname $(realpath $0))
 NODE_DIR=$(realpath $(pwd))
-NODE_MAINNET_DIR=${NODE_DIR}/mainnnetsingle
+NODE_MAINNET_DIR=${NODE_DIR}/mainnetsingle
 NODE_SOCKET_DIR=${NODE_MAINNET_DIR}/socket
 NODE_TRACER_SOCK=${NODE_SOCKET_DIR}/tracer.socket
 TMPFILE=$(mktemp tracerconfig.XXXXXXXXXX)
