636948f58a0a967ac479f107dfd9aa679a070b62
Nadia Chambers <nadia.chambers@iohk.io>
2025-08-27 12:18:50 +0200

    node script adjustments
    These don't work yet. I'll fix them later. For now, I'm just getting
    these up for Federico to see.

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,10 +1,54 @@
 #!/bin/sh
-exec cabal --jobs=256 run cardano-node -- \
-	run --config ./configuration/cardano/mainnet-config-new-tracing.json \
-		--database-path ./mainnetsingle/db \
-		--socket-path ./mainnetsingle/db/node.socket \
+
+CTF_SCRIPTS_DIR=$(dirname $(realpath $0))
+CTF_MAINNET_DIR=${CTF_SCRIPTS_DIR}
+CTF_CARDANO_DIR=${CTF_SCRIPTS_DIR}
+
+NODE_SCRIPTS_DIR=$(realpath $(pwd))
+NODE_EMPTY_DIR=${NODE_SCRIPTS_DIR}/empty_dir
+NODE_MAINNET_DIR=${NODE_SCRIPTS_DIR}/mainnetsingle
+NODE_CARDANO_DIR=${NODE_SCRIPTS_DIR}/cardano
+NODE_CONFIG_DIR=${NODE_SCRIPTS_DIR}/configuration
+
+NODE_ALONZO_GENESIS=${NODE_CONFIG_DIR}/cardano/mainnet-alonzo-genesis.json
+NODE_BYRON_GENESIS=${NODE_CONFIG_DIR}/cardano/mainnet-byron-genesis.json
+NODE_SHELLEY_GENESIS=${NODE_CONFIG_DIR}/cardano/mainnet-shelley-genesis.json
+NODE_CONWAY_GENESIS=${NODE_CONFIG_DIR}/cardano/mainnet-conway-genesis.json
+
+echo NODE_ALONZO_GENESIS=${NODE_ALONZO_GENESIS}
+echo NODE_BYRON_GENESIS=${NODE_BYRON_GENESIS}
+echo NODE_SHELLEY_GENESIS=${NODE_SHELLEY_GENESIS}
+echo NODE_CONWAY_GENESIS=${NODE_CONWAY_GENESIS}
+
+CONFIG_JSON=${CTF_CARDANO_DIR}/mainnet-config-new-tracing.json
+NODE_DB_PATH=${NODE_MAINNET_DIR}/db
+NODE_DB_SOCK=${NODE_DB_PATH}/node.socket
+TOPOLOGY_JSON=${CTF_CARDANO_DIR}/mainnet-topology.json
+TRACER_SOCK=${NODE_MAINNET_DIR}/socket/tracer.socket
+
+TMPFILE=/tmp/$(mktemp nodeconfig.XXXXXXXXXX)
+echo TMPFILE=${TMPFILE}
+JQ_ALONZO_CMD='.["AlonzoGenesisFile"]="'${NODE_ALONZO_GENESIS}'"'
+JQ_BYRON_CMD='.["ByronGenesisFile"]="'${NODE_BYRON_GENESIS}'"'
+JQ_SHELLEY_CMD='.["ShelleyGenesisFile"]="'${NODE_SHELLEY_GENESIS}'"'
+JQ_CONWAY_CMD='.["ConwayGenesisFile"]="'${NODE_CONWAY_GENESIS}'"'
+
+echo JQ_ALONZO_CMD=${JQ_ALONZO_CMD}
+echo JQ_BYRON_CMD=${JQ_BYRON_CMD}
+echo JQ_SHELLEY_CMD=${JQ_SHELLEY_CMD}
+echo JQ_CONWAY_CMD=${JQ_CONWAY_CMD}
+
+JQ_CMD=${JQ_ALONZO_CMD}' | '${JQ_BYRON_CMD}' | '${JQ_SHELLEY_CMD}' | '${JQ_CONWAY_CMD}
+echo JQ_CMD=${JQ_CMD}
+
+# jq ${JQ_ALONZO_CMD}' | '${JQ_BYRON_CMD}' | '${JQ_CONWAY_CMD} \
+
+jq "${JQ_CMD}" < ${CONFIG_JSON} > ${TMPFILE}
+exec cabal --jobs=256 --keep-going --disable-documentation run cardano-node -- \
+	run --config ${TMPFILE} \
+		--database-path ${NODE_DB_PATH} \
+		--socket-path ${NODE_DB_SOCK} \
 		--host-addr 0.0.0.0 \
 		--port 3001 \
-		--topology ./configuration/cardano/mainnet-topology.json \
-		--tracer-socket-path-connect \
-			./mainnetsingle/socket/tracer.socket
+		--topology ${TOPOLOGY_JSON} \
+		--tracer-socket-path-connect ${TRACER_SOCK}
--- scripts/tracer.sh
+++ scripts/tracer.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 SCRIPTS_DIR=$(dirname $(realpath $0))
-NODE_DIR=$(pwd)
+NODE_DIR=$(realpath $(pwd))
+NODE_MAINNET_DIR=${NODE_DIR}/mainnnetsingle
+NODE_SOCKET_DIR=${NODE_MAINNET_DIR}/socket
+NODE_TRACER_SOCK=${NODE_SOCKET_DIR}/tracer.socket
 TMPFILE=$(mktemp tracerconfig.XXXXXXXXXX)
-jq '.["network"].["contents"]="'${TMPFILE}'"' \
+jq '.["network"].["contents"]="'${NODE_TRACER_SOCK}'"' \
 	< ${SCRIPTS_DIR}/tracerconfig-mainnet.sh.json > ${TMPFILE}
 exec cabal --jobs=256 --keep-going --disable-documentation \
 	run cardano-tracer:exe:cardano-tracer -- \
