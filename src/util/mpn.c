607ca60cfa1f5f031afb8b1ebab30a00b4a19b24
Nadia Chambers <nadia.chambers@iohk.io>
2025-10-10 06:24:42 +0200

    use MPN_NR for array bounds

--- src/util/mpn.c
+++ src/util/mpn.c
@@ -3,7 +3,7 @@
 const char *
 mini_protocol_string(enum mini_protocol_num mpn)
 {
-	static char *mpn_string_table[9] = {
+	static char *mpn_string_table[MPN_NR] = {
 		[mpn_handshake]        = "mpn_handshake",
 		[mpn_EKG_metrics]      = "mpn_EKG_metrics",
 		[mpn_trace_objects]    = "mpn_trace_objects",
@@ -14,6 +14,7 @@ mini_protocol_string(enum mini_protocol_num mpn)
 		[mpn_state_query]      = "mpn_state_query",
 		[mpn_keepalive]        = "mpn_keepalive",
 	};
+	static char default_string[] = "unrecognized mpn";
 
-	return MPN_VALID(mpn) ? mpn_string_table[mpn] : NULL;
+	return MPN_VALID(mpn) ? mpn_string_table[mpn] : default_string;
 }
