f198aee13a43701ee4bac257dbe2b283db047c10
Nadia Chambers <nadia.chambers@iohk.io>
2025-11-26 16:00:25 +0100

    add cardano-node as a submodule for accessibility
    Also update the tracer script so runs on virgin cardano-node repos don't
    fail.

--- scripts/tracer.sh
+++ scripts/tracer.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 SCRIPTS_DIR=$(dirname $(realpath $0))
+# echo SCRIPTS_DIR=${SCRIPTS_DIR}
 NODE_DIR=${NODE_DIR:-$(realpath $(pwd))}
+# echo NODE_DIR=${NODE_DIR}
 NODE_MAINNET_DIR=${NODE_MAINNET_DIR:-${NODE_DIR}/mainnetsingle}
+# echo NODE_MAINNET_DIR=${NODE_MAINNET_DIR}
 NODE_SOCKET_DIR=${NODE_SOCKET_DIR:-${NODE_MAINNET_DIR}/socket}
 # NODE_SOCKET_DIR=${NODE_SOCKET_DIR:-${NODE_DIR}/run/current/tracer}
+# echo NODE_SOCKET_DIR=${NODE_SOCKET_DIR}
+if [ ! -d "${NODE_SOCKET_DIR}" ]
+then
+	echo Creating NODE_SOCKET_DIR=${NODE_SOCKET_DIR}
+	mkdir -p ${NODE_SOCKET_DIR}
+fi
 NODE_TRACER_SOCK=$(realpath ${NODE_SOCKET_DIR}/tracer.socket)
+# echo NODE_TRACER_SOCK=${NODE_TRACER_SOCK}
 TMPFILE=/tmp/$(mktemp tracerconfig.XXXXXXXXXX)
+# echo TMPFILE=${TMPFILE}
 if [ -v DEBUG ]
 then
 	echo NODE_TRACER_SOCK=${NODE_TRACER_SOCK}
